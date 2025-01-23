f2ddea3794f72f3ceb9a715989d8d1b68adc71c9
Tagle <gtaglea@hotmail.com>
2025-01-23 01:04:03 -0600

    registros de login y login exitoso

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,44 @@
 const AuthUser = require("../models/authUserModel");
+const bcrypt = require("bcrypt");
 
+const loginUser = async (req, res) => {
+  const { correo, contrasena } = req.body;
+
+  if (!correo || !contrasena) {
+    return res
+      .status(400)
+      .json({ error: "Correo y contraseña son obligatorios" });
+  }
+
+  try {
+    const usuario = await AuthUser.findOne({ correo });
+
+    if (!usuario) {
+      return res.status(404).json({ error: "Usuario no encontrado" });
+    }
+
+    const contrasenaValida = await bcrypt.compare(
+      contrasena,
+      usuario.contrasena
+    );
+
+    if (!contrasenaValida) {
+      return res.status(401).json({ error: "Contraseña incorrecta" });
+    }
+
+    return res.status(200).json({
+      message: "Inicio de sesión exitoso",
+      user: { correo: usuario.correo },
+    });
+  } catch (error) {
+    console.error("Error al iniciar sesión:", error);
+    return res.status(500).json({
+      error: "Ocurrió un error al procesar el inicio de sesión",
+    });
+  }
+};
+
+// Lógica de Registro
 const registerUser = async (req, res) => {
   const { correo, contrasena } = req.body;
 
@@ -25,4 +64,4 @@ const registerUser = async (req, res) => {
   }
 };
 
-module.exports = { registerUser };
+module.exports = { loginUser, registerUser };
