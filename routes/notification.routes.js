5881e36317ecc1dc539216112792f25d743c1ce2
Andres Gomez Tagle Azcarraga <eskayser@gmail.com>
2025-08-21 01:52:56 -0600

    reporte modelo 3

--- routes/notification.routes.js
+++ routes/notification.routes.js
@@ -357,35 +357,6 @@ router.post("/responder", async (req, res) => {
   }
 });
 
-
-
-// Schema actualizado (sin default pendiente)
-const mongoose = require('mongoose');
-
-const reportesSchema = new mongoose.Schema({
-  fraccId: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'fraccionamientos',
-    required: true
-  },
-  numeroCasa: { type: String, required: true },
-  nombre: { type: String, required: true },
-  motivo: { type: String, required: true },
-  foto: { type: String },
-  tiempo: { type: Date, default: Date.now }, 
-  estatus: {
-    type: String,
-    enum: ['aceptado', 'rechazado', 'cancelado', 'expirado'],
-    required: true
-  },
-  autorizadoPor: { type: String, required: true }, 
-  fechaAutorizacion: { type: Date, default: Date.now }
-});
-
-module.exports = mongoose.model("Reporte", reportesSchema, "reportes");
-
-// ===================================================================
-
 // Ruta para aceptar
 router.post('/:fraccId/notificacion/abrir-puerta', validarFraccionamiento, async (req, res) => {
   const { residenteId, reporteId, residenteNombre } = req.body;  
